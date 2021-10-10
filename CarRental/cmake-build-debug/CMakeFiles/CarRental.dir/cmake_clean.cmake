5e5e2ce742240f0c62c7defee5ec24d7591944d6
Camila Mastellini Oliveira <camila_mastellini@hotmail.com>
2021-10-10 20:41:50 -0300

    Adding CarRental

---
+++ CarRental/cmake-build-debug/CMakeFiles/CarRental.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarRental.dir/main.cpp.obj"
+  "CarRental.exe"
+  "CarRental.exe.manifest"
+  "CarRental.lib"
+  "CarRental.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarRental.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CarRental/main.cpp
@@ -0,0 +1,14 @@
+/*
+Rent a car:
+ Person to rent
+ Type of car
+ Price
+ Insurance
+ Total
+*/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
